f1bf8baaed38229acf689ee402188c1e82b8d364
thinh.dang <thinh.dang@skyblazestudios.com>
2023-09-20 13:37:36 +0700

    click page

--- client/src/components/SongCard.tsx
+++ client/src/components/SongCard.tsx
@@ -1,5 +1,5 @@
-import { FC } from 'react'
-import { Link } from 'react-router-dom'
+import { FC, MouseEvent } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 import PlayPause from './PlayPause'
@@ -26,13 +26,28 @@ const SongCard: FC<SongCardProps> = ({
   handlePauseClick,
   handlePlayClick,
 }) => {
+  const navigate = useNavigate()
   const { activeSong, isPlaying } = useSelector(selectPlayer)
+
+  const handlePlayPauseMusic = () => {
+    if (activeSong?.encodeId === encodeId && isPlaying) {
+      handlePauseClick()
+    } else {
+      handlePlayClick()
+    }
+  }
+
+  const showArtistDetail = (e: MouseEvent<HTMLSpanElement>, artistId: string) => {
+    e.stopPropagation()
+    navigate(`/artist/${artistId}`)
+  }
 
   return (
     <div
       className={`w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2 ${
         streamingStatus === 1 ? '' : 'pointer-events-none'
       } ${activeSong?.encodeId === encodeId ? 'bg-[#4c426e]' : 'bg-transparent'}`}
+      onClick={handlePlayPauseMusic}
     >
       <h3 className="font-bold text-xs text-white mr-3">{index + 1}</h3>
       <div className="flex-1 flex flex-row justify-between items-center w-full overflow-hidden">
@@ -45,20 +60,19 @@ const SongCard: FC<SongCardProps> = ({
               ?.map((artist: { alias: string; name: string }, index: number) => (
                 <span key={artist.name}>
                   {index > 0 ? <span>, </span> : ''}
-                  <Link to={`/artist/${artist?.alias}`} className="hover:underline">
+                  <span
+                    className="hover:underline"
+                    onClick={(e: MouseEvent<HTMLSpanElement>) => showArtistDetail(e, artist?.alias)}
+                  >
                     {artist?.name}
-                  </Link>
+                  </span>
                 </span>
               ))}
           </p>
         </div>
         {/* Show Song VIP */}
         <div className="text-yellow-500 font-medium mr-4">{streamingStatus === 1 ? '' : 'VIP'}</div>
-        <PlayPause
-          handlePauseClick={handlePauseClick}
-          handlePlayClick={handlePlayClick}
-          isPlaying={isPlaying && activeSong?.encodeId === encodeId}
-        />
+        <PlayPause isPlaying={isPlaying && activeSong?.encodeId === encodeId} />
       </div>
     </div>
   )
