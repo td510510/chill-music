9c47c4b624d8a156ca6435110fdbf928aa50b3d9
thinh.dang <thinh.dang@skyblazestudios.com>
2023-09-21 14:35:02 +0700

    add not found page

---
+++ client/src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="text-white text-base text-center h-[calc(100vh-72px)] absolute left-1/2 top-1/2 sm:translate-x-1/2 -translate-x-1/2">
+      404 | NotFound
+    </div>
+  )
+}
+
+export default NotFound
